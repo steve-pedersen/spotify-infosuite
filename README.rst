6ed729b6364f8b7b13542007201071fddc42b3c2
Steve Pedersen <sp@steves-mbp.gateway.sonic.net>
2017-11-05 17:53:21 -0800

    Added Spotify app connect, playback and artist/song info functionality.

--- README.rst
+++ README.rst
@@ -1,2 +1,5 @@
 # Spotify InfoSuite
 This is a class project at SF State, written by Andrew Lesondak and Steve Pedersen.
+
+Dependencies:
+pip3 install pytify
--- spotify_infosuite/controller/controller.py
+++ spotify_infosuite/controller/controller.py
@@ -1,4 +1,5 @@
 from model.frame import Frame
+from playback.playback import Playback
 from view.view_multi import ViewMulti
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QAction, QLineEdit
 from PyQt5.QtGui import QPixmap
@@ -7,12 +8,44 @@ from PyQt5.QtCore import *
 
 class Controller():
 
-    def __init__(self):
+	def __init__(self):
 
-        self.view_multi = ViewMulti(0, 0, 800, 600, "Spotify Info Suite", "view_multi")
+		self.view_multi = ViewMulti(0, 0, 800, 600, "Spotify Info Suite", "view_multi")
 
-        self.bio_frame = Frame(self, self.view_multi, 0, 100, 250, 400, "bio_frame")
-        self.bio_frame.set_display_text("mogwai was born in blah blah blah", 10, 10)
-        self.bio_frame.set_display_title("Bio", 5, 5)
-        self.view_multi.add_frame_bio(self.bio_frame)
-        self.view_multi.show()
+		self.bio_frame = Frame(self, self.view_multi, 0, 100, 250, 400, "bio_frame")
+		self.bio_frame.set_display_text("mogwai was born in blah blah blah", 10, 10)
+		self.bio_frame.set_display_title("Bio", 5, 5)
+		self.view_multi.add_frame_bio(self.bio_frame)
+		self.view_multi.show()
+
+		# init and open Spotify Desktop App
+		self.spotify = self.open_spotify()
+		print('Currently listening to: ', self.get_current_playing())
+
+
+
+	# Spotify Controls
+	def open_spotify(self):
+		spotify = Playback()
+		return spotify
+
+	def play_pause(self):
+		self.spotify.play_pause()
+
+	def next(self):
+		self.spotify.next()
+
+	def prev(self):
+		self.spotify.prev()
+
+	def pause(self):
+		self.spotify.pause()
+
+	def get_current_artist(self):
+		return self.spotify.get_current_artist()
+
+	def get_current_song(self):
+		return self.spotify.get_current_song()
+
+	def get_current_playing(self):
+		return self.get_current_artist() + ' - ' + self.get_current_song()
