5e92fcc934d3c8669487054cbf44790c315871d1
Steve Pedersen <sp@steves-mbp.gateway.sonic.net>
2017-11-05 18:08:45 -0800

    Trying playback without import

--- spotify_infosuite/controller/controller.py
+++ spotify_infosuite/controller/controller.py
@@ -1,10 +1,12 @@
 from model.frame import Frame
-from playback.playback import Playback
+# from playback.playback import Playback
 from view.view_multi import ViewMulti
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QAction, QLineEdit
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtMultimedia import QSoundEffect
 from PyQt5.QtCore import *
+import sys
+import subprocess
 
 class Controller():
 
@@ -19,7 +21,7 @@ class Controller():
 		self.view_multi.show()
 
 		# init and open Spotify Desktop App
-		self.spotify = self.open_spotify()
+		# self.spotify = self.open_spotify()
 		print('Currently listening to: ', self.get_current_playing())
 
 
@@ -47,5 +49,20 @@ class Controller():
 	def get_current_song(self):
 		return self.spotify.get_current_song()
 
+	# def get_current_playing(self):
+	# 	return self.get_current_artist() + ' - ' + self.get_current_song()
+
 	def get_current_playing(self):
-		return self.get_current_artist() + ' - ' + self.get_current_song()
+		instruction = ('on getCurrentTrack()\n'
+			' tell application "Spotify"\n'
+			'  set currentArtist to artist of current track as string\n'
+			'  set currentTitle to name of current track as string\n'
+			'  return currentArtist & " - " & currentTitle\n'
+			' end tell\n'
+			'end getCurrentTrack\n'
+			'getCurrentTrack()')
+		proc = subprocess.Popen(
+			['osascript', '-e', instruction],
+			stdout=subprocess.PIPE)
+		out, err = proc.communicate()
+		return out.decode(sys.getfilesystemencoding())
