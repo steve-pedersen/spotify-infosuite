fcc3bdd9326b4617f4159669f35e428894327ab5
Steve Pedersen <pedersen@dhcp-bur-130-212-68-187.sfsu.edu>
2017-11-29 09:30:46 -0800

    Added close button to popup

--- spotify_infosuite/view/single_frame.py
+++ spotify_infosuite/view/single_frame.py
@@ -39,8 +39,7 @@ class SingleFrameWindow(QWidget):
 
         def_width = 1000
         def_height = 900 if 900 < self.screen_h else self.screen_h - 50
-        self.w = def_width   
-        self.h = def_height   
+        self.w = def_width    
         self.h = frame.popup_text.height() if frame.popup_text.height() < self.screen_h else def_height
 
         frame.popup_text.setAlignment(Qt.AlignTop)
@@ -52,11 +51,19 @@ class SingleFrameWindow(QWidget):
         popup_layout = QVBoxLayout(self)
         popup_layout.addWidget(popup_scroll)
        
+        close_btn = QPushButton('x', self)
+        close_btn.move(self.w-25, 0)
+        close_btn.resize(25, 20)
+        close_btn.setObjectName('close_popup_btn')
+        close_btn.clicked.connect(self.close_popup)
+
         self.setGeometry(self.x, self.y, self.w, self.h)
         self.load_styles()
 
         frame.show()
 
+    def close_popup(self):
+        self.close()
 
     # Opens css stylesheet and apply it to Spotify Infosuite elements
     def load_styles(self):
